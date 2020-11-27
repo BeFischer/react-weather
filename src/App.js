64bed683711e91022d7bc8ded831b5e185404015
Be Fischer <be_fischer@yahoo.com>
2020-11-27 15:59:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <div>
         <header></header>
         <div className="container-fluid">
         <Search />
                </div>
-        <footer>Coded by Be Fischer & available in<a href="https://github.com/BeFischer/react-weather" >GitHub</a></footer>
-      </div>
+        <footer>Coded by Be Fischer & available in{""}<a href="https://github.com/BeFischer/react-weather" >GitHub</a></footer>
     </div>
   );
 }
